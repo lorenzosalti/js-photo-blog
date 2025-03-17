f1ce976e480e11aaa1895b8aed74be863bb6a82e
lorenzosalti <lorenzosalti@gmail.com>
2025-03-17 15:55:57 +0100

    feat: selected picture in overlay

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ const gridContainerElement = document.getElementById('grid');
 // overlay elements
 const overlayElement = document.querySelector('.overlay');
 const olCloseElement = document.querySelector('.close-btn');
+const olImageElement = document.getElementById('ol-image');
 
 // API endpoint url
 const picsEndpoint = 'https://lanciweb.github.io/demo/api/pictures/';
@@ -37,11 +38,37 @@ axios.get(picsEndpoint)
     // array containing every card
     const cardsElements = document.querySelectorAll('.card-body');
 
+    console.log('cardsElement: ', cardsElements);
+
     // iteration adding a click event on every card
     cardsElements.forEach((element) => {
+
+      // gets the image container element
+      const imageElement = element.firstElementChild;
+
+      console.log('imageElement: ', imageElement);
+
+      // gets the ID of the image
+      const imageID = imageElement.id.toString();
+
+      console.log('imageID: ', imageID);
+
+      // click evento on the card
       element.addEventListener('click', function () {
         overlayElement.classList.add('active');
+
+        // gets the current image by ID
+        const currentImageElement = document.getElementById(imageID);
+
+        console.log('currentImageElement: ', currentImageElement);
+
+        // adds the innerHTML (img tag) of the current image
+        olImageElement.innerHTML = currentImageElement.innerHTML;
+
+        console.log('olImageElement.innerHTML: ', olImageElement.innerHTML);
+
       });
+
     });
 
   })
@@ -49,6 +76,7 @@ axios.get(picsEndpoint)
     const status = error.status;
     console.log(`Error ${status}`);
   });
+
 
 // click event on close button
 olCloseElement.addEventListener('click', function () {
@@ -68,11 +96,11 @@ function generateHTML(objArray) {
   // iteration on data array
   obj.forEach((element) => {
 
-    const { title, date, url } = element;
+    const { id, title, date, url } = element;
 
     gridContainerElement.innerHTML += `
     <div class="card-body">
-      <div class="card-image">
+      <div class="card-image" id="${id}">
         <img src="${url}" alt="${title}">
       </div>
       <h6>${date}</h6>
